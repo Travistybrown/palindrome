9f9069651c7c718e10ac674a1154d0ccacd533a0
Travistybrown <travistbrownsc@gmail.com>
2022-10-13 09:17:34 -0400

    dsfdfssdffds

--- js/site.js
+++ js/site.js
@@ -9,34 +9,50 @@ if (userString == "") {
       text: "Make a input in the bar!",
     });
   } else {
-    let palindrome = checkPalindrom(userString);
-    displayString(palindrome);
+    let returnObj = checkPalindrom(userString);
+    displayString(returnObj);
   }
 }
 
 
     function checkPalindrom(userString) {   
+        const regex = /[^a-z0-9]/gi;
      userString = userString.toLowerCase();
-    userString = userString.replace(' ',' ')
+    userString = userString.replace(regex, '')
 
     let palindrome = [];
+    let returnObj = {}
 
     for (let i = userString.length - 1; i >= 0; i-- ){
     palindrome += userString[i];
  }
     if(palindrome == userString){
-    palindrome = "Is a palindrome";
+    returnObj.msg = "well done! You entered a Plaindrome!" ;
  }  else{
-    palindrome = "Is NOT a palindrome";
+     returnObj.msg = "Sorry! You did not enter a Palindrome!"  ;
  }
-    return palindrome ;
+    returnObj.reversed = palindrome ;
+
+    return returnObj
 
 }
 
 
-function displayString(palindrome) {
-  document.getElementById(
-    "msg"
-  ).innerHTML = `Your string reversed is: <strong>${palindrome}</strong>`;
-  document.getElementById("alert").classList.remove("invisible");
+function displayString(returnObj) {
+ let palAlert = document.getElementById("alert");
+ let alertHdr = document.getElementById("alertHeader");
+ if (returnObj.msg.includes("Sorry")) {
+   palAlert.classList.remove("alert-success");
+   palAlert.classList.add("alert-danger");
+ } else {
+   palAlert.classList.remove("alert-danger");
+   palAlert.classList.add("alert-success");
+ }
+ //write to the page
+ alertHdr.innerHTML = returnObj.msg;
+ document.getElementById("msg").innerHTML = `Your phrase reversed is: <strong>${returnObj.reversed}</strong>`;
+ //show the alert box
+ palAlert.classList.remove("invisible");
+
 }
+
